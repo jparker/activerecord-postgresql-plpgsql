ba2454799f3a12162999080324e1aa5a988db52e
John Parker <jparker@urgetopunt.com>
2015-08-28 15:29:09 -0700

    clean up some comments

--- lib/activerecord/postgresql/plpgsql.rb
+++ lib/activerecord/postgresql/plpgsql.rb
@@ -1,7 +1,7 @@
 require 'active_record/connection_adapters/postgresql_adapter'
 
 ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
-  # create_function(:do_a_thing, as:<<SQL, returns: :trigger)
+  # create_function :name_of_function, as:<<SQL, returns: :trigger
   # BEGIN
   #   -- ...
   #   -- PL/pgSQL function body
@@ -16,13 +16,13 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
       " $PROC$ LANGUAGE plpgsql"
   end
 
-  # remove_function(:do_a_thing)
+  # remove_function :name_of_function
   def remove_function(name, if_exists: false, cascade: false)
     command = if_exists ? 'DROP FUNCTION IF EXISTS' : 'DROP FUNCTION'
     execute "#{command} #{name}() #{'CASCADE' if cascade}"
   end
 
-  # create_trigger(:do_a_thing, before: [:insert, :update], on: :widgets)
+  # create_trigger :name_of_trigger, before: [:insert, :update], on: 'table_name'
   def create_trigger(name, on:, before: nil, after: nil, execute: name, replace: false)
     if before && !Array(before).empty?
       context = "BEFORE #{Array(before).map(&:upcase).join(' OR ')}"
@@ -32,13 +32,13 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
       raise ArgumentError, 'must provide either :before or :after keyword argument'
     end
 
-    remove_trigger(name, on: on) if replace
+    remove_trigger name, on: on, if_exists: true if replace
     execute \
       "CREATE TRIGGER #{name} #{context} ON #{on}" \
       " FOR EACH ROW EXECUTE PROCEDURE #{execute}()"
   end
 
-  # remove_trigger(:do_a_thing, on: :widgets)
+  # remove_trigger :name_of_trigger, on: 'table_name'
   def remove_trigger(name, on:, if_exists: false)
     command = if_exists ? 'DROP TRIGGER IF EXISTS' : 'DROP TRIGGER'
     execute "#{command} #{name} ON #{on}"
